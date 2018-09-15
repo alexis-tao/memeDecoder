4c619cffad1e4e75305ccfa1c0bd9d6f6fc7d48b
Alexis S. Tao <tao.alexis@gmail.com>
2018-09-15 11:12:34 -0400

    implement image uploading and changed styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state= { 
+      uploadedFileCloudinaryURL: ''
+    };
+
+    this.onImageDrop = this.onImageDrop.bind(this);
+  }
+
+  onImageDrop(files){
+    this.setState({ 
+      newImage: files[0]
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="app">
+        <p className="appTitle">meme decoder</p>
+        <p className="text">
+          give us a meme and we'll decode it for you!
         </p>
+        <Dropzone
+          multiple={false}
+          accept="image/*"
+          onDrop={this.onImageDrop.bind(this)}
+        >
+          <img className="uploadedImage" src={ this.state.newImage && this.state.newImage.preview && this.state.newImage.preview }/>
+        </Dropzone>
       </div>
     );
   }
