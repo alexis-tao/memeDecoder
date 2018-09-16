36cfa207937dc935acfaf34e6a1c9e627789689a
Alexis S. Tao <tao.alexis@gmail.com>
2018-09-16 01:53:39 -0400

    added side header and change styles

--- src/App.js
+++ src/App.js
@@ -52,41 +52,49 @@ class App extends Component {
   render() {
 
     return (
-      <div className="app">
-        <p className="appTitle">what does it meme?</p>
-        <p className="text">
-          give us a meme, we'll give you meaning of the meme 
-        </p>
-        <Dropzone
-          multiple={false}
-          accept="image/*"
-          onDrop={this.onImageDrop.bind(this)}
-          className="dropzone"
-        >
-          { !this.state.newImage && 
-            <p className="smallText">+ click to upload or drag an image here</p>
-          }
-          { this.state.newImage && <img alt="upload" className="uploadedImage" src={ this.state.newImageURL }/> }
-        </Dropzone>
-        { this.state.isImageUploaded &&
-          <div>
-          <p>meaning of your meme</p>
-          { this.state.newImage.name === "testBrain.jpeg" &&
-            <ul className='memeMeaning'>
-              <li>this is known as an <strong>expanding brain meme</strong></li>
-              <li>often used in irony to assoicate situations with intellect (size of the brain)</li>
-              <li>learn more about <a href="https://knowyourmeme.com/memes/expanding-brain">expanding brain memes</a></li>
-            </ul>
-          }
-          { this.state.newImage.name === "testStarterPack.jpg" &&
-            <ul className='memeMeaning'>
-              <li>this is known as a <strong>starter pack meme</strong></li>
-              <li>often used to illustrate common characteristics of a certain group</li>
-              <li>learn more about <a href="https://knowyourmeme.com/memes/starter-packs">starter pack memes</a></li>
-            </ul>
-          }
+      <div className="appWrapper">
+        <div className="header">
+          <img alt='meme' className="logo" src='meme.png'/>
+          <a className="headerLink" href="https://en.wikipedia.org/wiki/Meme">what is a meme?</a>
+          <a className="headerLink" href="https://mashable.com/article/2018-best-memes/#M7q.TUik.Pqd">classic memes</a>
+          <a className="headerLink" href="https://fsmedia.imgix.net/c7/92/42/c6/34dd/4002/b05c/71637eda680b/qdhl3sgpldcj0jdlsa8fqhdixl6wr1xq--fe1dn59c8jpg.jpeg?auto=format%2Ccompress&w=650">meme of the day</a>
+        </div>
+        <div className="app">
+          <div className="appTitle">what does it meme?</div>
+          <div className="appDescription">
+            give us a meme, we'll give you meaning of the meme 
           </div>
-        }
+          <Dropzone
+            multiple={false}
+            accept="image/*"
+            onDrop={this.onImageDrop.bind(this)}
+            className="dropzone"
+          >
+            { !this.state.newImage && 
+              <div className="smallText">+ click to upload or drag an image here</div>
+            }
+            { this.state.newImage && <img alt="upload" className="uploadedImage" src={ this.state.newImageURL }/> }
+          </Dropzone>
+          { this.state.isImageUploaded &&
+            <div>
+              <div className="meaningTitle">meaning of your meme</div>
+              { this.state.newImage.name === "testBrain.jpeg" &&
+                <ul className='memeMeaning'>
+                  <li>this is known as an <strong>expanding brain meme</strong></li>
+                  <li>often used in irony to assoicate situations with intellect (size of the brain)</li>
+                  <li>learn more about <a href="https://knowyourmeme.com/memes/expanding-brain">expanding brain memes</a></li>
+                </ul>
+              }
+              { this.state.newImage.name === "testStarterPack.jpg" &&
+                <ul className='memeMeaning'>
+                  <li>this is known as a <strong>starter pack meme</strong></li>
+                  <li>often used to illustrate common characteristics of a certain group</li>
+                  <li>learn more about <a href="https://knowyourmeme.com/memes/starter-packs">starter pack memes</a></li>
+                </ul>
+              }
+            </div>
+          }
+        </div>
       </div>
     );
   }
