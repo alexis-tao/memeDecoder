5b62e42080a20d959da7a35de33bbbcde2bafb25
Alexis S. Tao <tao.alexis@gmail.com>
2018-09-16 01:15:27 -0400

    change styles and added placeholder text

--- src/App.js
+++ src/App.js
@@ -22,10 +22,29 @@ class App extends Component {
       isImageUploaded: true,
     });
 
-    this.decodeMeme(this.state.newImageURL);
+    //this.decodeMeme(this.state.newImageURL);
   }
 
   decodeMeme(fileURL){
+
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  client
+    .labelDetection('./resources/wakeupcat.jpg')
+    .then(results => {
+      const labels = results[0].labelAnnotations;
+
+      console.log('Labels:');
+      labels.forEach(label => console.log(label.description));
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 
   }
 
@@ -34,7 +53,7 @@ class App extends Component {
 
     return (
       <div className="app">
-        <p className="appTitle">meme decoder</p>
+        <p className="appTitle">what does it meme?</p>
         <p className="text">
           give us a meme, we'll give you meaning of the meme 
         </p>
@@ -45,13 +64,27 @@ class App extends Component {
           className="dropzone"
         >
           { !this.state.newImage && 
-            <p className="smallText">Click to upload or drag an image here</p>
+            <p className="smallText">+ click to upload or drag an image here</p>
           }
-          { this.state.newImage && <img alt="image upload" className="uploadedImage" src={ this.state.newImageURL }/> }
+          { this.state.newImage && <img alt="upload" className="uploadedImage" src={ this.state.newImageURL }/> }
         </Dropzone>
         { this.state.isImageUploaded &&
-          <div className='memeMeaning'>
-          <p>here are possible meanings of your meme:</p>
+          <div>
+          <p>meaning of your meme</p>
+          { this.state.newImage.name === "testBrain.jpeg" &&
+            <ul className='memeMeaning'>
+              <li>this is known as an <strong>expanding brain meme</strong></li>
+              <li>often used in irony to assoicate situations with intellect (size of the brain)</li>
+              <li>learn more about <a href="https://knowyourmeme.com/memes/expanding-brain">expanding brain memes</a></li>
+            </ul>
+          }
+          { this.state.newImage.name === "testStarterPack.jpg" &&
+            <ul className='memeMeaning'>
+              <li>this is known as a <strong>starter pack meme</strong></li>
+              <li>often used to illustrate common characteristics of a certain group</li>
+              <li>learn more about <a href="https://knowyourmeme.com/memes/starter-packs">starter pack memes</a></li>
+            </ul>
+          }
           </div>
         }
       </div>
